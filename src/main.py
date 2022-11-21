bdcca99a84b288531579550ac28d7cd6fcc74f4c
ice036 <ice036@qq.com>
2022-11-21 09:22:03 +0800

    something small change

--- src/main.py
+++ src/main.py
@@ -1,10 +1,10 @@
-from plus import ky_cctalk_plus, ky_download_plus
+from plus import cctalk_plus, download_plus
 
 if __name__ == '__main__':
     # 第一种使用方法
-    ky_cctalk_plus.get_by_course_id('1612582780690810')
-    # ky_download_plus.download(ky_cctalk_plus.movie_list, 26)
+    cctalk_plus.get_by_course_id('1612582780690810')
+    # download_plus.download(cctalk_plus.movie_list, 26)
 
     # 第二种使用方法
-    # ky_cctalk_plus.get_by_movie_id('16444799633133')
-    # ky_download_plus.download(ky_cctalk_plus.movie_list, 0, seq=False)
+    # cctalk_plus.get_by_movie_id('16444799633133')
+    # download_plus.download(cctalk_plus.movie_list, 0, seq=False)
